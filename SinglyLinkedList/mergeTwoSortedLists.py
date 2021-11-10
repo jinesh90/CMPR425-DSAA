2a317041864e1bf526a684b3c3a4fcd4941d01a8
jinesh90 <jinesh90@gmail.com>
2021-11-09 23:01:51 -0800

    merging two sorted linked list: recursion

--- SinglyLinkedList/mergeTwoSortedLists.py
+++ SinglyLinkedList/mergeTwoSortedLists.py
@@ -15,27 +15,49 @@ s3 = slist()
 head1 = s1._first
 head2 = s2._first
 
-while head1 != None or head2 != None:
-    if head1 != None:
-        val1 = head1.val
+# while head1 != None or head2 != None:
+#     if head1 != None:
+#         val1 = head1.val
+#     else:
+#         val1 = 1000
+#     if head2 != None:
+#         val2 = head2.val
+#     else:
+#         val2 = 1000
+#
+#     if val1 >= val2:
+#         s3.append(val2)
+#         head2 = head2.next
+#     else:
+#         s3.append(val1)
+#         head1 = head1.next
+#
+#
+# print(s3)
+
+
+
+"""
+by using recursion
+"""
+
+def merge_list(head1,head2):
+    if head1 is None:
+        return head2
+    elif head2 is None:
+        return head1
+    elif head1.val < head2.val:
+        head1.next = merge_list(head1.next, head2)
+        return head1
     else:
-        val1 = 1000
-    if head2 != None:
-        val2 = head2.val
-    else:
-        val2 = 1000
-
-    if val1 >= val2:
-        s3.append(val2)
-        head2 = head2.next
-    else:
-        s3.append(val1)
-        head1 = head1.next
+        head2.next = merge_list(head2.next, head1)
+        return head2
 
 
-print(s3)
+head1 = s1._first
+head2 = s2._first
 
-
-
-
-
+n = merge_list(head1, head2)
+while n != None:
+    print(n.val)
+    n = n.next
