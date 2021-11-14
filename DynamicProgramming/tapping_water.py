582fd755690bc1b37a23a4bc473574c3eb73eb3c
jinesh90 <jinesh90@gmail.com>
2021-11-13 16:49:36 -0800

    adding solution for tapping water leetcode #42

---
+++ DynamicProgramming/tapping_water.py
@@ -0,0 +1,28 @@
+"""
+leet code problem 42
+dynamic programming method
+"""
+
+
+def find_tapping_water(height):
+    ans = 0
+    if len(height) == 0:
+        return 0
+    left_max = [None] * len(height)
+    right_max = [None] * len(height)
+    left_max[0] = height[0]
+    for i in range(1, len(height)):
+        left_max[i] = max(height[i], left_max[i-1])
+
+    right_max[len(height)-1] = height[len(height)-1]
+    for j in range(len(height)-2, -1, -1):
+        right_max[j] = max(height[j], right_max[j+1])
+
+    for k in range(1, len(height)):
+        ans += min(left_max[k],right_max[k]) - height[k]
+    return ans
+
+
+print(find_tapping_water([0,1,0,2,1,0,1,3,2,1,2,1]))
+
+
---
+++ Leetcode/tapping_water.py
@@ -0,0 +1,22 @@
+"""
+leet code problem 42
+brute force method
+"""
+
+def find_tapping_water(a):
+    ans = 0
+    for i in range(0,len(a)):
+        left_max = 0
+        right_max = 0
+        for j in range(i, -1, -1):
+            left_max = max(left_max, a[j])
+        for j in range(i, len(a)):
+            right_max = max(right_max, a[j])
+        ans += min(left_max, right_max) - a[i]
+    return ans
+
+
+print(find_tapping_water([4,2,0,3,2,5]))
+
+
+
