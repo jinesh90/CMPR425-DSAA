bb2582e0ff0c0fdbd42a3b695f25acf5821a65df
jinesh90 <jinesh90@gmail.com>
2021-11-18 00:45:41 -0800

    longest substring, deque, balancing bucket, container with the most water.

--- Leetcode/longest_substring.py
+++ Leetcode/longest_substring.py
@@ -3,30 +3,6 @@ leetcode problem # 3 find longest substring.
 """
 
 
-
-# def char_repeat(s, start, end):
-#     char_array = [0] * 128
-#     for i in range(start, end+1):
-#         c = s[i]
-#         char_array[ord(c)] += 1
-#         if char_array[ord(c)] > 1:
-#             return False
-#     return True
-#
-#
-# def find_longest_brute_force(s):
-#     """
-#
-#     :param s:
-#     :return:
-#     """
-#     max_len = 0
-#     for i in range(len(s)):
-#         for j in (i, len(s)):
-#             if char_repeat(s, i, j):
-#                 max_len = max(max_len, j-i+1)
-#     return max_len
-
 def find_longest_brute_force(s):
     def check(start, end):
         chars = [0] * 128
--- Leetcode/longest_substring_2.py
+++ Leetcode/longest_substring_2.py
@@ -3,7 +3,21 @@ leetcode problem # 3 find longest substring.
 using two pointers and hash
 """
 
+def longest_substring(s):
+    m = dict()
+    result = 0
+    i = 0
+    n = len(s)
+    substrings = []
+    for j in range(n):
+        if s[j] in m:
+            i = max(m[s[j]], i)
+        result = max(result, j - i + 1)
+        substrings.append(s[i:j+1])
+        m[s[j]] = j + 1
+    print(substrings)
+    return result
 
 
-
-
+s = "abcabcbbafghai"
+print(longest_substring(s))
